1ed24820c5d58f7752b0cb099729a43febdf094f
qafarlicavid <javidig@code.edu.az>
2022-11-20 18:07:48 +0400

    Fin Code validation is ready

---
+++ Emloyee Management System/Emloyee Management System/Atributes/FinValidation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Emloyee_Management_System.Atributes
+{
+    public class FinnValidation : ValidationAttribute
+    {
+
+        public override bool IsValid(object? value)
+        {
+            var fin = value.ToString();
+
+            string regexed = @"^([A-Z0-9]{7}$)";
+
+            return Regex.IsMatch(fin, regexed);
+
+
+        }
+    }
+}
