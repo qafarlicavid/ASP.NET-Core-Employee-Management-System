d075dd8c9f52281d8d87b7ad50e98c0bce42e1a5
qafarlicavid <javidig@code.edu.az>
2022-11-20 18:06:23 +0400

    Employee's List page is ready

---
+++ Emloyee Management System/Emloyee Management System/ViewModels/ListViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Emloyee_Management_System.ViewModels
+{
+    public class ListViewModel
+    {
+        public string EmployeeCode { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public bool IsDeleted { get; set; }
+
+       
+        public ListViewModel(string employeeCode, string firstName, string lastName, string fatherName, bool isDeleted)
+        {
+            EmployeeCode = employeeCode;
+            FirstName = firstName;
+            LastName = lastName;
+            FatherName = fatherName;
+            IsDeleted = isDeleted;
+        }
+    }
+}
