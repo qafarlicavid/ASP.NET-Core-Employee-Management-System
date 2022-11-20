1b0346c4c8e72265f981fa70eadbdbe79805f91f
qafarlicavid <javidig@code.edu.az>
2022-11-20 17:57:03 +0400

    Employee properties is created

---
+++ Emloyee Management System/Emloyee Management System/Database/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Emloyee_Management_System.Database.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmployeeCode { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public string Fin { get; set; }
+        public string Email { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
